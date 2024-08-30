83f162f4c6a296cfd3aee08205f27be2f6c9de33
Shubham Dwivedi <dwivedishubham545@gmail.com>
2024-08-31 00:53:52 +0530

    loader update

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -8,7 +8,7 @@ const Loader = () => {
       <Circles
       height="80"
       width="80"
-      color="##6f6af8"
+      color="#6f6af8"
       ariaLabel="circles-loading"
       wrapperStyle={{}}
       wrapperClass=""

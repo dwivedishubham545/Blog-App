f5e127de111129b84110a675245367429dbd7167
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2024-08-30 23:15:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://blog-app-shubham.vercel.app/" }));
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 

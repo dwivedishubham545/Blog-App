9d826ba71a77382ead52c15c6a637857dbd13078
Shubham Dwivedi <dwivedishubham545@gmail.com>
2024-08-31 00:46:38 +0530

    css updates

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import LoadingGif from '../images/loading.gif'
+import React from "react";
+import { Circles } from "react-loader-spinner";
+// import LoadingGif from '../images/loading.gif'
 
 const Loader = () => {
   return (
     <div className="loader">
-        <div className="loader__image">
-            <img src={LoadingGif} alt="Loading..." />
-        </div>
+      <Circles
+      height="80"
+      width="80"
+      color="##6f6af8"
+      ariaLabel="circles-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
